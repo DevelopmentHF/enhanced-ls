1d32cf0a165ba682637f26f552f5002444e36d7c
developmentHF <development.henry@gmail.com>
2023-04-15 22:22:49 +1000

    ls but only for working dir of els file

--- els.c
+++ els.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
 
 int
 main(int argc, char** argv) {
 
+    // Open directory of ../enhanced-ls
+    DIR *dir = opendir(".");
+    struct dirent *ent;
+
+    // Go through all entries within this directory
+    if (dir != NULL) {
+        while ((ent = readdir(dir)) != NULL) {
+            // Abide by ignore philosophy
+            if (ent->d_name[0] != '.') {
+                printf("%s\n", ent->d_name);
+            }
+        }
+        closedir(dir);
+    } else {
+        printf("Error: could not open directory.\n");
+    }
     return 0;
 }
